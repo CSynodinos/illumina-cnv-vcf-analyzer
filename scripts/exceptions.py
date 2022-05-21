be9f857083570fee7ccf413b31622f0bd2e8c906
Christos Synodinos <chris_sinodinos@outlook.com>
2022-05-21 06:47:57 +0100

    added InputFileError custom exception

--- scripts/exceptions.py
+++ scripts/exceptions.py
@@ -36,3 +36,20 @@ class NotCnvError(Exception):
             return '{0} '.format(self.errmessage)
         else:
             return 'NotCnvError has been raised'
+
+class InputflError(Exception):
+    """Custom exception class for incorrect input files."""
+
+    __module__ = 'builtins'
+
+    def __init__(self, *args) -> None:
+        if args:
+            self.errmessage = args[0]
+        else:
+            self.errmessage = None
+
+    def __repr__(self) -> str:
+        if self.errmessage:
+            return '{0} '.format(self.errmessage)
+        else:
+            return 'InputflError has been raised'
